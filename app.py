af1822bd10620204cfcbed276b6aa4e8d302732a
aneeshphatak7 <39762740+aneeshphatak7@users.noreply.github.com>
2020-09-20 18:53:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 app = dash.Dash()
+server=app.server
 app.layout = html.Div([
     html.H1('Hello Dash!'),
     html.Div('Dash-aframewprk')
