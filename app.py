0b1421aa6c50d9ece3f8d718dfd8efbca9cf8b31
aneeshphatak7 <39762740+aneeshphatak7@users.noreply.github.com>
2020-09-22 04:02:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,11 @@ from mlxtend.frequent_patterns import association_rules
 from mlxtend.preprocessing import TransactionEncoder
 import plotly.graph_objs as go
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-url = 'https://raw.githubusercontent.com/aneeshphatak7/Dash-app/master/df4.csv'
+url='https://raw.githubusercontent.com/aneeshphatak7/Dash-app/master/df4.csv'
 empData = pd.read_csv(url,sep=",")
 colnames = ['antecedents', 'consequents', 'antecedent support',
        'consequent support', 'support', 'confidence', 'lift', 'leverage',
        'conviction']
-
 records = []
 for i in range(0,len(empData)):
     records.append([str(empData.values[i,j]) 
@@ -30,56 +29,57 @@ te = TransactionEncoder()
 te_ary = te.fit(records).transform(records)
 df = pd.DataFrame(te_ary, columns=te.columns_)    
 
+
+
 def SupervisedApriori(df,consequent,min_supp,min_conf,min_lift,max_length,sort_by):
 
         
 
     frequent_itemsets = apriori(df, min_support=min_supp, use_colnames=True,max_len=max_length)
-    frequent_itemsets=frequent_itemsets.sort_values(by='support',ascending=False)
     rules = association_rules(frequent_itemsets, metric="lift", min_threshold=min_lift)
-    #rules.head()
     rules=rules[(rules['confidence'] > min_conf)&(rules['consequents']=={consequent})].sort_values(by='confidence',ascending=False)
-    #sup_rules=rules
 
-    #rules.reindex()
-    print(rules)
     return(rules)
+
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+
 app.layout = html.Div(children=[
-    html.H1('Employee Attrition: Association Rules Mining',style=dict(color='Orange',align='center',justify='center')),
+    html.H1('Change following parameters to observe changes in top rules and plot',style=dict(color='Orange',align='center',justify='center')),
+     html.Br([]),
+     html.Br([]),
 
    html.Div([ html.Div(children='''
-        Min_Confidence:     
+        Min Confidence:     
     ''',
-    style={'width':'15%','display': 'inline-block'
+    style={'width':'10%','display': 'inline-block'
             }),
     html.Div(children='''
-        Min_Support:     
+        Min Support:     
     ''',
-    style={'width':'15%','display': 'inline-block'
+    style={'width':'10%','display': 'inline-block'
             }),
     html.Div(children='''
-        Min_Lift:     
+        Min Lift:     
     ''',
-    style={'width':'15%','display': 'inline-block'
+    style={'width':'10%','display': 'inline-block'
             }),
     html.Div(children='''
         Consequents:     
     ''',
-    style={'width':'15%','display': 'inline-block'
+    style={'width':'10%','display': 'inline-block'
             }),
     html.Div(children='''
         Max_length:     
     ''',
-    style={'width':'15%','display': 'inline-block'
+    style={'width':'10%','display': 'inline-block'
             }),
     html.Div(children='''
         Sort_by:     
     ''',
-    style={'width':'15%','display': 'inline-block'
+    style={'width':'10%','display': 'inline-block'
             }),
  
  html.Div([
@@ -87,14 +87,14 @@ dcc.Input(
     placeholder='Enter Confidence: ',
     type='number',
     value=0.7,
-    id = 'confidence',style={'width':'15%','display': 'inline-block'
+    id = 'confidence',style={'width':'10%','display': 'inline-block'
             }
 ) ,
 dcc.Input(
     placeholder='Enter Support',
     type='number',
     value=0.4,
-    id = 'support',style={'width':'15%','display': 'inline-block'
+    id = 'support',style={'width':'10%','display': 'inline-block'
             }
 ) ,
 
@@ -102,7 +102,7 @@ dcc.Input(
     placeholder='Enter Lift',
     type='number',
     value=1,
-    id = 'lift',style={'width':'15%','display': 'inline-block'
+    id = 'lift',style={'width':'10%','display': 'inline-block'
             }
 ) ,        
 dcc.Dropdown(
@@ -110,7 +110,7 @@ dcc.Dropdown(
     options = [
         {'label':'No','value':'Attrition=No'},{'label':'Yes','value':'Attrition=Yes'}],
     value='Attrition=No',
-    id = 'Consequent', style={'width':'200px','display': 'inline-block'
+    id = 'Consequent', style={'width':'100px','display': 'inline-block'
             }
 ) ,        
 
@@ -118,7 +118,7 @@ dcc.Input(
     placeholder='Max_lenth',
     type='number',
     value=4,
-    id = 'rules', style={'width':'15%','display': 'inline-block'
+    id = 'rules', style={'width':'10%','display': 'inline-block'
             }
 ) ,  
 dcc.Dropdown(
@@ -126,7 +126,7 @@ dcc.Dropdown(
     options = [
         {'label':'lift','value':'lift'},{'label':'confidence','value':'confidence'},{'label':'support','value':'support'}],
     value='lift',
-    id = 'Sort_by', style={'width':'200px','display': 'inline-block'
+    id = 'Sort_by', style={'width':'150px','display': 'inline-block'
             }
 ) , 
    html.Button('Submit', id='submit-button-state', n_clicks=0),
@@ -134,7 +134,7 @@ dcc.Dropdown(
     ]),
       
 html.Div(id="table1"),]),
-html.H3("Assosiation rules Plot ",style=dict(color='Blue',align='center',justify='center')),
+html.H3("Scatterplot for custom rules ",style=dict(color='Orange',align='center',justify='center')),
         html.Div(id='graphs'),
 
 ])
@@ -166,14 +166,14 @@ def update_datatable(n_clicksx,confidence,support,lift,rules,Consequent,Sort_by)
     
     print("++++++++++++++++++++++++++++++++++++++")
     print(sup_rules.to_dict("rows"))
-    tab=html.Div(children=[html.Br([]),html.Label(children='Top 5 rules', style={'width': '50%', 'display': 'inline-block',
-                                                                 'margin': 0, 'padding': '8px','color':'blue'}),
+    tab=html.Div(children=[html.Br([]),html.Label(children='Top 5 rules', style={'width': '60%', 'display': 'inline-block',
+                                                                 'margin': 0, 'padding': '10px','color':'blue'}),
             dash_table.DataTable(
                     id='table',
                     columns=[{"name": i, "id": i} for i in sup_rules.columns],
                     data=sup_rules.to_dict("rows"),
-                    style_cell={'width': '300px',
-                    'height': '60px',
+                    style_cell={'width': '250px',
+                    'height': '70px',
                     'textAlign': 'left'})
                     ])
     return tab
@@ -199,20 +199,18 @@ def update_graph(nclick,confidence,support,lift,rules,Consequent,Sort_by):
     sup=rules['support']
     conf=rules['confidence']
     lif=rules['lift']
-    #sup_rules = rules[['support','confidence','lift']]
     print(rules["support"])
     print(rules["confidence"])
     print(rules["lift"])
-#    b = sns.scatterplot(data=rules,x='support',y='confidence',hue='lift')
-#    plt.show()
+
     return html.Div([
             dcc.Graph(
-        id='ScatterPlot Confidence Vs Support',
+        id='ScatterPlot of Confidence Vs Support',
         figure={
                 'data':[
                         go.Scatter(x=sup,
                          y= conf,
-                         #color=lif,
+                        
                          
                          mode='markers',
                          marker=dict(
@@ -222,7 +220,7 @@ def update_graph(nclick,confidence,support,lift,rules,Consequent,Sort_by):
                         ],
                 
                 'layout': go.Layout(
-                        title ='Scatterplot of Confidence vs Support with lift as color gradient'
+                        title ='Scatterplot of Confidence vs Support for custom rules'
                         
                         )
                 }
